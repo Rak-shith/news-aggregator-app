a2006657e881d6246d03f8aad594b21b79565250
Rakshith <kotian412@gmail.com>
2024-12-27 10:01:17 +0530

    code changes

--- src/service/newsCredAPI.jsx
+++ src/service/newsCredAPI.jsx
@@ -3,12 +3,8 @@ import { normalizeNewsData } from "../utils/normalizeNewsData";
 
 export const fetchMediaStackAPI = async (searchText) => {
   try {
-    const response = await axios.get(`https://api.mediastack.com/v1/news`, {
-      params: {
-        access_key: import.meta.env.VITE_MEDIASTACK_API_KEY,
-        keywords: searchText,
-        countries: "in,us,gb"
-      }
+    const response = await axios.get(`https://content.guardianapis.com/search?q=${searchText}`, {
+      params: { "api-key": import.meta.env.VITE_MEDIASTACK_API_KEY }
     });
     return normalizeNewsData(response.data.data, "MediaStack");
   } catch (error) {
--- src/utils/normalizeNewsData.jsx
+++ src/utils/normalizeNewsData.jsx
@@ -2,12 +2,13 @@ import noImage from "../assets/noImage.png";
 
 export const normalizeNewsData = (data, source) => {
   return data.map(item => ({
-    title: item?.title || item.headline?.main || item?.name || item.snippet,
+    title: item?.title || item?.webTitle || item.headline?.main || item?.name || item.snippet,
     description: item?.description || item?.abstract || item?.lead_paragraph || "No description available",
-    url: item?.url || item.web_url,
+    url: item?.url || item?.webUrl || item.web_url,
     image: item?.image || item?.urlToImage || item?.multimedia?.[0]?.url || noImage,
-    published_at: item?.published_at || item?.publishedAt || item?.pub_date || "No date available",
-    source: source,
-    category: item?.category || "general"
+    published_at: item?.published_at || item?.webPublicationDate || item?.publishedAt || item?.pub_date || "No date available",
+    source: source || item?.source || "Reuters",
+    category: item?.category || item?.sectionName || "general",
+    author: item?.author || item?.byline?.original || "John Doe",
   }));
 };
